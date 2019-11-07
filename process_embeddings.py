e67cae09efeaa37f4e1c6073acd458980ae2b1b3
Piotr <piotrek.janus96@gmail.com>
2019-11-07 20:46:21 +0100

    meta file changes

--- process_embeddings.py
+++ process_embeddings.py
@@ -4,10 +4,20 @@ import numpy as np
 
 def save_model(person_vec, file_name):
     with open(f'{file_name}-emb.tsv','w', encoding='utf-8') as vec_file, open(f'{file_name}-meta.tsv','w', encoding='utf-8') as metafile:
+        metafile.write('Imie\ttyp\tzawod'+'\n')
         for entity, embeddings in person_vec:
-            vec = '\t'.join(map(str, embeddings))
-            vec_file.write(vec+'\n')
-            metafile.write(entity+'\n')
+            try:
+                vec = '\t'.join(map(str, embeddings))
+                entity_vec = [list(k.values()) for k in people if k['name'] == entity]
+                if len(entity)>0:
+                    entity_vec = entity_vec[0]
+                    entity_vec = '\t'.join(map(str, entity_vec))
+                else:
+                    entity_vec = entity+'\t \t '
+                vec_file.write(vec+'\n')
+                metafile.write(entity_vec+'\n')
+            except:
+                pass
 
 def prepare_vector(embed, context='single', window=5):
     person_vec = []
@@ -66,15 +76,13 @@ def prepare_vector(embed, context='single', window=5):
             ## Get all documents
             docs = list(embed[person].values())
             corp_sent = []
-            ## For doc in docs
+            ## For doc in docs 
             for doc in docs:
                 ## For sent in doc
                 for sent in doc:
                     if isinstance(sent, list):
                         before = sent[0]
                         after = sent[1]
-                        if len(before) == 0 and len(after) == 0:
-                            continue
                         vec = before[:window] + after[:window]
                         vec = [v.numpy() for v in vec]
                         if len(vec)<2:
@@ -91,12 +99,11 @@ def prepare_vector(embed, context='single', window=5):
     return person_vec
 
 if __name__ == "__main__":
+    from tqdm import tqdm
     with open(FILE_NAME, 'rb') as f:
         embed = pickle.load(f)
 
-    for context in ['single', 'document', 'corpus']:
-        for window in [1, 2, 3, 4, 5]:
+    for context in ['single','document','corpus']:
+        for window in tqdm([1,2,3,4,5]):
             to_save = prepare_vector(embed, context=context, window=window)
             save_model(to_save, f'{context}-{window}')
-
-
